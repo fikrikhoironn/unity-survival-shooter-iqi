2bb9c0312755c87c956a35479dea5d5638e4a188
graceclaudia19 <13520078@std.stei.itb.ac.id>
2023-03-28 16:05:54 +0700

    base code

---
+++ Assets/Scripts/Managers/EnemyFactory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+public class EnemyFactory : MonoBehaviour, IFactory
+{
+
+    [SerializeField]
+    public GameObject[] enemyPrefab;
+
+    public GameObject FactoryMethod(int tag, Vector3 pos, Quaternion rot)
+    {
+        GameObject enemy = Instantiate(enemyPrefab[tag], pos, rot);
+        return enemy;
+    }
+}
---
+++ Assets/Scripts/Managers/GameOverManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+using UnityEngine.UI;
+
+public class GameOverManager : MonoBehaviour
+{
+    public Text warningText;
+    public PlayerHealth playerHealth;
+    private bool playerDead;
+
+    Animator anim;
+
+    void Awake()
+    {
+        playerDead = false;
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (playerHealth.currentHealth <= 0 && !playerDead)
+        {
+            anim.SetTrigger("GameOver");
+            playerDead = true;
+        }
+    }
+
+    public void ShowWarning(float enemyDistance)
+    {
+        warningText.text = string.Format("! {0} m", Mathf.RoundToInt(enemyDistance));
+        anim.SetTrigger("Warning");
+    }
+}
