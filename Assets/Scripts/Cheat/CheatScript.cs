f00d3e457ab06bc038b1fb51153fe4c3b1a28149
fikrikhoironn <fikrikf470@gmail.com>
2023-04-12 08:35:13 +0700

    feat: new CheatScript.cs logic

--- Assets/Scripts/Cheat/CheatScript.cs
+++ Assets/Scripts/Cheat/CheatScript.cs
@@ -5,16 +5,44 @@ using UnityEngine.UI;
 
 public class CheatScript : MonoBehaviour
 {
-    public InputField cheatInput;
-    public Wallet wallet;
-    public GameObject Player;
-  
+    public GameObject player;
+    private bool isCheatInputVisible = false;
+    private InputField cheatInput;
+
+    void Start()
+    {
+        // Get the InputField component
+        cheatInput = GetComponent<InputField>();
+        // Hide the InputField initially
+        // cheatInput.gameObject.SetActive(false);
+    }
+
     void Update()
     {
-        if (cheatInput.text == "suicide")
+        if (Input.GetKeyDown(KeyCode.Q))
         {
-            Player.SendMessage("Suicide");
+            Debug.Log("BackQuote pressed");
+            isCheatInputVisible = !isCheatInputVisible;
+            cheatInput.gameObject.SetActive(isCheatInputVisible);
+            if (isCheatInputVisible)
+            {
+                cheatInput.Select();
+                cheatInput.text = "";
+            }
         }
 
+        if (cheatInput.gameObject.activeSelf && Input.GetKeyDown(KeyCode.Return))
+        {
+            if (cheatInput.text == "suicide")
+            {
+                PlayerHealth playerHealth = player.GetComponent<PlayerHealth>();
+                playerHealth.TakeDamage(playerHealth.currentHealth);
+            }
+            else if (cheatInput.text == "money")
+            {
+                Wallet wallet = player.GetComponent<Wallet>();
+            }
+            cheatInput.text = "";
+        }
     }
 }
